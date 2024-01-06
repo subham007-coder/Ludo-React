83ee3153bd5187b55615958a4e5b8b7d84e9459d
subham007-coder <dsubham109@gmail.com>
2024-01-06 21:03:20 +0530

    add

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,14 +1,31 @@
+import { useState } from "react";
+
 export default function TodoList() {
+    let [todos, setTodos] = useState(["sampale todo"]);
+    let [newTodos, NewSetTodos] = useState();
+
+    let updateTodoValue = (e) => {
+        NewSetTodos(e.target.value);
+    }
+
+    let addNewTask = () => {
+
+    }
+
     return (
         <>
             <div>
-                <input type="text" placeholder="add a task" />
+                <input type="text" value={newTodos} onChange={updateTodoValue} placeholder="add a task" />
                 <br /><br />
-                <button>Add Task</button>
+                <button onClick={addNewTask}>Add Task</button>
                 <br /><br />
                 <h4>Todo List</h4>
                 <ul>
-                    <li></li>
+                    {
+                        todos.map((todo) => (
+                            <li>{todo}</li>
+                        ))
+                    }
                 </ul>
             </div>
         </>
